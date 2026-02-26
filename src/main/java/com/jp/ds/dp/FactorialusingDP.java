40d6d42afa8eefcb31a391c360bd9b2da7f8df0d
Bala Kumar Narayanan <bala850@gmail.com>
2026-02-26 14:03:38 +0530

    Factorial by recursion

---
+++ src/main/java/com/jp/ds/dp/FactorialusingDP.java
@@ -0,0 +1,16 @@
+package com.jp.ds.dp;
+
+public class FactorialusingDP {
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+
+        int[] dp = new int[n+1];
+
+        return 0;
+    }
+}
